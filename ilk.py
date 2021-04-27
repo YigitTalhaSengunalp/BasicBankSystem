da8d7f99b2b83113d45c5937fdb06f109479c544
Yiğit Talha Şengünalp <83014930+YigitTalhaSengunalp@users.noreply.github.com>
2021-04-27 18:09:25 +0300

    Add files via upload

---
+++ ilk.py
@@ -0,0 +1,16 @@
+print("Welcome To Maze Bank. Please Register System")
+
+Ad = input("Write Your Name Surname" " : ")
+yas = input("Write Your Age" " : ")
+
+bilgiler = [Ad,yas]
+
+print("Congulatulations You Are Registered Maze Bank Succesfully")
+print("Your Informations" " : " ,(Ad), (yas),)
+
+print("Money" " : " "313131 Dollar")
+
+paraCek = int(input(" How Many Money You Want To Transfer ? " " : "))
+toplamBakiye = 313131
+
+print("Your Money Is Transfered Succesfully Thanks For Choosing Us",int(toplamBakiye)-int(paraCek))
